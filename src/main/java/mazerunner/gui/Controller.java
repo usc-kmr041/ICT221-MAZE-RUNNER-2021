8aac0a0866f63febad8dcce479ececb93bcc9111
Chester <kmr041@student.usc.edu.au>
2021-05-12 17:50:17 +1000

    started on gui

--- src/main/java/mazerunner/gui/Controller.java
+++ src/main/java/mazerunner/gui/Controller.java
@@ -1,6 +1,48 @@
 package mazerunner.gui;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import mazerunner.engine.GameEngine;
+
+import java.awt.event.MouseEvent;
+
 public class Controller {
 
+    public void moveup(javafx.scene.input.MouseEvent mouseEvent) {
+        System.out.println("up");
+    }
+    public void movedown(javafx.scene.input.MouseEvent mouseEvent) {
+        System.out.println("down");
+    }
+    public void moveleft(javafx.scene.input.MouseEvent mouseEvent) {
+        System.out.println("left");
+    }
+    public void moveright(javafx.scene.input.MouseEvent mouseEvent) {
+        System.out.println("right");
+    }
 
+
+
+
+
+    public class GameGUI extends Application {
+        // TODO: move this to Controller class if you use FXML...
+        private GameEngine engine = new GameEngine(10);
+
+
+        @Override
+        public void start(Stage primaryStage) throws Exception {
+            Parent root = FXMLLoader.load(getClass().getResource("game_gui.fxml"));
+            //Button root = new Button("Amazing Miner Game coming soon...");
+            //root.setFont(new Font(24));
+
+            primaryStage.setScene(new Scene(root, 800, 600));
+            primaryStage.setTitle("Maze Runner Game");
+            primaryStage.setResizable(true);
+            primaryStage.show();
+        }
+    }
 }
